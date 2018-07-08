e509f1b5b2c328e6cf0fc4cf70a33e0d84ab526f
Cristal Alatorre Perez <cristalcodes@gmail.com>
2018-07-08 02:01:42 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,31 @@
+var chocolateBars=['snickers', 'hundredgrand', 'kitkat', 'skittles'];
+
+function addElementToBeginningOfArray(array, element){
+  return [element,...array]
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element){
+  array.unshift(element)
+  return array
+}
+
+function addElementToEndOfArray(array, element){
+  return [...array, element]
+}
+
+function destructivelyAddElementToEndOfArray(array, element){
+  array.push(element)
+  return array
+}
+
+function accessElementInArray(array, index){
+  var array=[1,2,3]
+  return array[2]
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  array.shift()
+  return array
+}
+
+function removeElementFromEndOfArray()
