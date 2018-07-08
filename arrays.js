d9705132786957029eee59180f2ed846dc28bac9
Cristal Alatorre Perez <cristalcodes@gmail.com>
2018-07-08 02:14:11 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,8 @@
 var chocolateBars=['snickers', 'hundredgrand', 'kitkat', 'skittles'];
 
 function addElementToBeginningOfArray(array, element){
-  return [element,...array]
+  var array=[1]
+  return ['foo',...array]
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element){
